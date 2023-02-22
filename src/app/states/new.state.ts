2e236df3a09e861a6654bc251be71a073b59a7ac
minhnn2006 <123917156+minhnn2006@users.noreply.github.com>
2023-02-22 07:53:39 +0700

    Update new.state.ts

--- src/app/states/new.state.ts
+++ src/app/states/new.state.ts
@@ -1,7 +1,7 @@
 import {New} from "rc/app/models/news.model"
 export interface New{
     loading: boolean,
-    loaded:boolean,
-    error:boolean,
-    news:New[]
+    isSuccess:boolean,
+    error:string,
+    news:New[];
 }
