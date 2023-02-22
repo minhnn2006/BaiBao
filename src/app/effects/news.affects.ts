48fd65d65e8e16514ed93a1eb1b533ac801594eb
minhnn2006 <123917156+minhnn2006@users.noreply.github.com>
2023-02-22 08:22:51 +0700

    Update news.affects.ts

--- src/app/effects/news.affects.ts
+++ src/app/effects/news.affects.ts
@@ -1,1 +1,24 @@
+import {Injectable}
+import {Actions}
 
+@Injectable()
+  export class New{
+  apiURL="https://social.runwayclub.dev/api/articles/latest?page=1&per_page=3"
+  constructor(private actions: Actions, private http: HttpCilent){
+
+  }
+  getNews$=createEffect(()=>{
+    return this.actions.pipe(
+        ofType(NewActions.getNews)
+        switchMap((actions)=> {
+            return this.http.get(this.apiURL)
+        }),
+        map((response:any)=>{
+            return NewActions.getNewsSuccess({news:response })
+        }),
+        catchError((error)=>{
+            return of(NewActions.getNewsFailure({error:error}))
+        })
+    )
+  })
+}
