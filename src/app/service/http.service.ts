9db09b880a6b0e37742f5de784743449178130ad
minhnn2006 <123917156+minhnn2006@users.noreply.github.com>
2023-02-22 08:31:59 +0700

    Update http.service.ts

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -7,12 +7,12 @@ import { map } from 'rxjs';
 })
 export class HttpService {
 
-  constructor(private httpClient: HttpClient) { }
-  get(){
-    let url = 'https://social.runwayclub.dev/api/articles/latest'
-    return this.httpClient.get(url).pipe(map((data: any) => {
-        console.log(data);
-        return <News>data;
-    }))
-  }
+  //constructor(private httpClient: HttpClient) { }
+ // get(){
+  //  let url = 'https://social.runwayclub.dev/api/articles/latest'
+  //  return this.httpClient.get(url).pipe(map((data: any) => {
+    //    console.log(data);
+    //    return <News>data;
+  //  }))
+ // }
 }
