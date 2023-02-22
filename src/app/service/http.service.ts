ced96aae5732720d05046fba3aa4f54ef3f7888d
minhnn2006 <123917156+minhnn2006@users.noreply.github.com>
2023-02-22 08:30:59 +0700

    Update http.service.ts

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { News } from '../models/news.model';
+
 import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
