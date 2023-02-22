a9499713556ad296d40b00a24d2883e423daa25b
minhnn2006 <123917156+minhnn2006@users.noreply.github.com>
2023-02-22 07:52:13 +0700

    Update news.model.ts

--- src/app/models/news.model.ts
+++ src/app/models/news.model.ts
@@ -1,18 +1,7 @@
-import { User } from '@angular/fire/auth';
-import { article } from "./Article.model.";
 
-export interface News{
 
-    tags: string;
-    user:{
-        name: string
-        username: string
-        twitter_username: string
-        github_username: string
-        website_url: string
-        profile_image: string
-        profile_image_90: string
-    }
-    
-    articles: article[];
+export interface New{
+  id:number,
+  title:string,
+  discription:string,
 }
