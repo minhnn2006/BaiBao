78317eab582ea5acedddab1299f4710ebca4391e
minhnn2006 <123917156+minhnn2006@users.noreply.github.com>
2023-02-22 08:21:58 +0700

    Update new.action.ts

--- src/app/actions/new.action.ts
+++ src/app/actions/new.action.ts
@@ -3,6 +3,6 @@ import New from 'src/app/models/news.models'
 
 export const NewActions={
   getNews:createAction('[New] get all new'),
-getNewsSuccess:createAction('[New] get all new success', props<{news:New[]}>()),
+getNewsSuccess:createAction('[New] get all new success', props<{news:Array<any>}>()),
   getNewsFailure:createAction('[New] get all new success',props<{error:string}>()),
 }
